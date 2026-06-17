890cc68aad96b4260f47e93b50c144b808c5a051
azzahra-windows <azzahraarmelia14@gmail.com>
2026-06-17 17:41:10 +0700

    Initial commit

---
+++ src/test/java/hooks/Hooks.java
@@ -0,0 +1,25 @@
+package hooks;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Hooks {
+    public static WebDriver driver;
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) driver.quit();
+    }
+}
